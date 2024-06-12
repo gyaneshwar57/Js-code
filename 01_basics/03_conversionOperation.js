202a6c46768ecc00022c3821b8c9b09633170b9c
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-12 14:25:33 +0000

    conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,10 +13,29 @@ let valueInnumber= Number(score)
 
 let isLoggedin=1  //
 let booleanIsLoggedin= Boolean(isLoggedin)
-console.log(booleanIsLoggedin)
+//console.log(booleanIsLoggedin)
 // 1 = true  0=false   ""=false    "Gyani"=true
 
 
 let someNumber= 33
 let stringNumber=String(someNumber)
-console.log(typeof stringNumber) // output=string
+// console.log(typeof stringNumber) // output=string
+ 
+let value =3
+let negValue= -value
+//console.log(negValue)
+//console.log(2+2)
+
+let str1= "Gyani"
+let str2= " Hello"
+let str3= str1 + str2
+//console.log(str3)
+//console.log("1"+2)
+//console.log(1+"2")
+//console.log("1"+2+2)
+//console.log(1+2+"2")
+
+//console.log(+true) output=1 //if in (+"")  output=0
+let gameCounter= 100
+gameCounter++;  //if it is ++gameCounter then output will be same i.e, 101
+console.log(gameCounter)
