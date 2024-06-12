432ada1ce678e6ab2ab55accf19ec98be75a201a
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-12 07:08:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let name="Gyani"  //string
+let age=23 //number
+let isLoggedin = true //bool
+let state; 
+console.log(name)
+
+
+
+console.log(typeof age) //number
+console.log(typeof name)//string
+console.log(typeof null)// object
+console.log(typeof undefined) //undefined
