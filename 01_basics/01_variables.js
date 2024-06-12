7a59d6b5764bb6d09fb151a2c312f2ef90f338d4
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-12 05:05:28 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accId=123
+let accEmail="hc@gmail.com"
+accCity ="hyd"
+let accState;
+
+accEmail="h@sc.com"
+accCity="mumbai"
+
+console.log(accId);
+console.table([accId,accEmail,accCity,accState])
