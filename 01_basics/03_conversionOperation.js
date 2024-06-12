e4f8771a6ec3b8ea602a562118d9b5453560761a
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-12 08:11:43 +0000

    conversionNumber

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = null //if true then it will be (boolean,number,1)
+                 //if undefined the (undefined,number,Nan)
+                 //"33abc"=(string,number,Nan)
+                 //"33"=(string,number,33)
+//console.log(typeof score)
+ 
+
+let valueInnumber= Number(score)
+//console.log(typeof valueInnumber)
+//console.log(valueInnumber)
+
+
+
+let isLoggedin=1  //
+let booleanIsLoggedin= Boolean(isLoggedin)
+console.log(booleanIsLoggedin)
+// 1 = true  0=false   ""=false    "Gyani"=true
+
+
+let someNumber= 33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber) // output=string
