2a155c4b2e9fa7e11ef79179bd64120c8d0275ca
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-18 11:32:38 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -3,7 +3,7 @@ console.log(score) // 400
 const balance= new Number(100)
 console.log(balance) // [number: 400]
 
-console.log(balance.toString().length)// changed to string i.e, 400
+console.log(balance.toString().length)// 3    //.toString() changed to string i.e, 400
 
 console.log(balance.toFixed(2)); //400.00
 
@@ -21,7 +21,7 @@ console.log(Math.abs(-4)) // 4
 console.log(Math.round(4.6))  //4
 console.log(Math.min(4,5,6,7))// 4
 
-console.log(Math.random())//   gives value between 0 and 
+console.log(Math.random())//   gives value between 0 and 1
 console.log((Math.random()*10)+1);//  6.1755153477456
 //console.log(Math.floor(Math.random()*10)+1)// 7
 
