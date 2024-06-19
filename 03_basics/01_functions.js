5528230d56fca730256f299fb47fc9fae97b6d1a
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-19 17:36:54 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,10 +1,10 @@
 
 function sayMyname(){
-    console.log("G");
-    console.log("y");
-    console.log("a");
-    console.log("n");
-    console.log("i");
+   // console.log("G");
+    //console.log("y");
+    //console.log("a");
+    //console.log("n");
+    //console.log("i");
 }
 //sayMyname()
 
@@ -29,7 +29,42 @@ function loginUserMessage(username){// if username="sam"  then output: sam just 
     return `${username} just logged in`
 }
 //console.log(loginUserMessage("Gyani"))
-console.log(loginUserMessage("Gyani"));
+//console.log(loginUserMessage("Gyani"));
+
+function calculateCartPrice(val1,val2, ...num1){ ///rest operator ...
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500,600));//[200,400,500,600] val1,val2, ...num1
+
+
+const user={
+    username:"Gyani",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and ${anyobject.price}`);
+}
+
+
+//handleObject(user) // username is Gyani and price is 199
+handleObject({
+    username:"sam",
+    price : 399
+})                  // username is sam and price is 399
+
+const myNewArray=[200,400,100,600]
+function returnSecondValue(getArray){
+   return myNewArray[2]
+}
+console.log(returnSecondValue(myNewArray));         //400
+//console.log(returnsecondValue([200,400,500,100]))// 400
+
+
+
+
+
+
 
 
 
