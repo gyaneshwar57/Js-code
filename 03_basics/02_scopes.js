b8f27049a1c37f4fb04038e62e811b122ebeb833
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-19 17:58:24 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+//var c=300  //30
+let a=300
+if(true){
+    let a=10
+    const b=20
+    console.log("INNER:",a); //10
+}
+
+
+
+console.log(a);  //300
+//console.log(b);// b not defined for using var
+//console.log(c);
+
+
+
+
+
+
+
+
+
+
