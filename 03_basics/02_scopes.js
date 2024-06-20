1d2d2d532c03a306f4721dcb74295b1c7f56209d
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-20 02:47:47 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,6 +13,40 @@ console.log(a);  //300
 //console.log(c);
 
 
+function one(){
+    const username="Gyani"
+    function two(){
+        const website="youtube"
+        //console.log(username);
+    }
+    //console.log(website);// web site not dfined
+    two()// Gyani
+}
+//one() //if we comment out two() it will not execute
+
+if(true){
+    const username="Gyani"
+    if(username==="GYani"){
+        const website="youtube"
+        //console.log(username+website); //Gyani youtube
+    }
+    //console.log(website); not defined
+}
+//console.log(username);   not defined
+
+//+++++++++++++++++++++++++++ interesting+++++++++++++++++++
+addone(5)//it will get 6 if use console log
+function addone(num){
+    return num+1
+}
+
+
+adTwo(5) // not defined
+const adTwo=function(num2){
+    return num+2
+}
+
+
 
 
 
