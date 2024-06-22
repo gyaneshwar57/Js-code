84529f655f191fa28430b9349456266cd2d73057
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-22 02:26:15 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,42 @@
+//if
+const isUserloggedIn=true
+const temperature=41
+//if(temperature<50){    
+//  console.log("less than 50");
+//}else{
+//  console.log("temp is greater than 50");
+//}
+//console.log("execute");  //compulsory executes
+
+
+
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`)// user power: fly
+// }
+// console.log(`user power:${power}`); //error
+
+// const balance =1000
+// //if(balance>500) console.log("test");
+// if(balance<500){
+//     console.log("less than 500");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+// if(userLoggedIn && debitCard){
+//     console.log("allow to buy course");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("user logged in");// user logged in
+// }
+
+
+
