313e1e61ec064d5086184e8ffcf8268737faf427
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-06-22 02:38:37 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,39 @@
+// witch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month=3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");// mar
+        break;
+    case 4:
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
