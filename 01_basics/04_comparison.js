181d131ac3b888f267d4f17977aa53ef59acff7c
gyaneshwar57 <110961316+gyaneshwar57@users.noreply.github.com>
2024-08-03 08:39:05 +0530

    Delete 01_basics directory

--- 01_basics/04_comparison.js
+++
@@ -1,6 +0,0 @@
-console.log("2">1)// true because it automatically converts to number "2"
-console.log(null<0)// false null converts to a number treating as 0
-console.log(null<0) // false
-console.log(null>=0) // true
-// incase of undefined it is always false when we comapare
-// console.log("2" === 2) // it returns false    === stritly comparison
